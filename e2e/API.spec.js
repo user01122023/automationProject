c9b5c736ab937eb9c839af2859e0da0c0cd10cf6
ana-creator <59834914+ana-creator@users.noreply.github.com>
2023-12-13 14:18:59 +0000

    multi

--- e2e/API.spec.js
+++ e2e/API.spec.js
@@ -8,7 +8,7 @@ test.describe('API testing', () => {
 
   test('Post Request - Login', async ({ request }) =>{
     const response = await request.post(`${baseUrl}`, {
-      data: {
+      multipart: {
         "username": 'Default',
         "key": '2yiZaG7QEQaHNvmJJbHrluqeX0u3YsCuL1lEssKr05Jvl3zUHVkSIJ16ajZXIrBNcPVB6P2Tkn2PFC2ABHqBTlLADD6TsFu2YytngyynjLpEr6kzbsTjmRJM4kUoMmslm4D6okv7ylxKxrzBNe216O3dMaBZ1S1yBdajD08fF6yQzMQHT3vCoOJ6baPLBFrzoQqwnZuhacRQQFa1LbpOKOKAKtxZoI4XLNcucnBOu5aOXdBHhUJ2VatArE6MLiZi'
 
@@ -17,7 +17,7 @@ test.describe('API testing', () => {
       }
       
     })
-    const responseBody = JSON.parse(await response.text())
+    //const responseBody = JSON.parse(await response.text())
     expect(response.status()).toBe(200)
     expect(response.ok()).toBeTruthy()
     console.log(await response.json())
@@ -25,14 +25,14 @@ test.describe('API testing', () => {
  
   })
 
-  test.only('customer', async ({ request }) =>{
+  test('customer', async ({ request }) =>{
 
     const response = await request.post(`${baseUrl}`, {
       params:{
         "route" : "api/customer",
         "api_token": "3d6d55c0b42fb7ca43e5683bff"
       },
-      data:{
+      multipart:{
         "firstname":"Dear",
         "lastname":"Customer",
         "email":"customer@example.com",
@@ -53,14 +53,15 @@ test.describe('API testing', () => {
         "route" : "api/currency",
         "api_token": "3d6d55c0b42fb7ca43e5683bff"
       },
-      data:{
+      multipart:{
         "currency": "USD"        
       }
     })
     expect(response.status()).toBe(200)
     expect(response.ok()).toBeTruthy()
-    console.log(typeof await response.body()) // Should print 'object'
-    console.log(await response.body()) // Should print the object
+    //console.log(typeof await response.body()) // Should print 'object'
+    //console.log(await response.body()) // Should print the object
+    console.log(await response.json())
     //console.log(response.json()) // Should print the object
     //expect(response.body).toEqual({"success": "Success: Your currency has been changed!" });
        
